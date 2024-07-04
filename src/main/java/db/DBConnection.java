d9dd89072c586a87e804548e4655dc65644b6569
Jose Contreras <jmcontreras.stoltze@gmail.com>
2024-07-03 20:26:07 -0400

    Commit test segundo - corrección credencial

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -15,9 +15,9 @@ public class DBConnection {
     }
 
     // private static final String url = "jdbc:mysql://localhost:3306/Evaluacion2";
-    
+    // private static final String user = "root";
     private static final String url = "jdbc:mysql://10.0.2.15:3306/Evaluacion2";
-    private static final String user = "root";
+    private static final String user = "root_test";
     private static final String password = "Jmcstolz99@";
 
     public static Connection getConnection() throws SQLException {
